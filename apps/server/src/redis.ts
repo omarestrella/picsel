741f856b690de52f9281eb5c5e9686c95a435ad6
Omar Estrella <oyestrella91@gmail.com>
2023-04-09 21:20:42 -0400

    Redis using env vars

--- apps/server/src/redis.ts
+++ apps/server/src/redis.ts
@@ -1,7 +1,8 @@
 import { connect, type Redis } from "https://deno.land/x/redis@v0.29.2/mod.ts";
 
 export const redis = await connect({
-  hostname: "127.0.0.1",
+  hostname: Deno.env.get("REDIS_HOST") ?? "localhost",
+  password: Deno.env.get("REDIS_PASSWORD") ?? "",
   port: 6379,
 });
 
